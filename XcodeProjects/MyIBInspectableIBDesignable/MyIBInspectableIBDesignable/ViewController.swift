c4166feabd338d2eb3fa8742688ac8ff984c1a59
Taeho Gu <sbpdt1062@gmail.com>
2022-06-17 00:10:07 +0900

    Add files via upload

---
+++ XcodeProjects/MyIBInspectableIBDesignable/MyIBInspectableIBDesignable/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  MyIBInspectableIBDesignable
+//
+//  Created by 구태호 on 2022/06/16.
+//
+
+
+// MARK: @IBInspector 및 @IBDesignable 사용법에 대한 예제
+// MARK: Main.storyboard의 inspector 창 및 CocobeanView.swift 파일 참조
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
