828701fc93cf3f6459b8dccf21b28c886023ba11
gutaeho <sbpdt1062@gmail.com>
2022-09-15 11:48:49 +0900

    프로젝트 디렉토리 정리

--- MyAlamoFire/Pods/Target Support Files/Pods-MyAlamoFire/Pods-MyAlamoFire-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_MyAlamoFire : NSObject
-@end
-@implementation PodsDummy_Pods_MyAlamoFire
-@end
--- MySnapKit/Pods/Target Support Files/Pods-MySnapKit/Pods-MySnapKit-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_MySnapKit : NSObject
-@end
-@implementation PodsDummy_Pods_MySnapKit
-@end
