120e21386b85a7504b1c7cf0910e46bffa9f61f4
gutaeho <sbpdt1062@gmail.com>
2023-03-09 03:08:55 +0900

    - 휴대전화 번호 하이픈 포함 정규식 체크 개선

--- Project/MyRegex/MyRegex/util/RegExUtil.swift
+++ Project/MyRegex/MyRegex/util/RegExUtil.swift
@@ -11,7 +11,9 @@ class RegExUtil {
     /// 이메일
     private static let emailEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
     /// 01로 시작 다음 0,1,7 가능 + (7~8 자리) 0 ~ 9 숫자
-    private static let phoneEx = "^01[0-1, 7][0-9]{7,8}$"
+    private static let phoneEx = "^01[0-1, 7][0-9]{3,4}[0-9]{4}$"
+    /// 하이픈이 포함된 전화번호 일 경우
+    private static let phoneExWithHyphen = "^01([0|1|6|7|8|9]?)-?([0-9]{3,4})-?([0-9]{4})$"
     /// (8~30자리) 영문 + 숫자 + 특수 문자
     private static let passwordEx = "^(?=.*[A-Za-z])(?=.*[0-9])(?=.*[!@#$%^&*()_+=-]).{8,30}"
     
@@ -25,14 +27,18 @@ class RegExUtil {
     }
     
     /// 휴대전화 정규식 체크
-    /// - note: 하이픈이 포함된 문자일 경우 제거한 뒤 비교
+    /// - note: 하이픈이 포함된 문자일 경우 포함
     static func isPhoneNumber(phoneNumber: String?) -> Bool {
         guard let phoneNumber = phoneNumber, phoneNumber != "" else { return false }
-        // 하이픈 제거
-        let phoneNumberRemovedHyphen = phoneNumber.components(separatedBy: ["-"]).joined()
         
-        if phoneNumberRemovedHyphen.range(of: phoneEx, options: .regularExpression) != nil {
-            return true
+        if phoneNumber.contains("-") {
+            if phoneNumber.range(of: phoneExWithHyphen, options: .regularExpression) != nil {
+                return true
+            }
+        } else {
+            if phoneNumber.range(of: phoneEx, options: .regularExpression) != nil {
+                return true
+            }
         }
         return false
     }
