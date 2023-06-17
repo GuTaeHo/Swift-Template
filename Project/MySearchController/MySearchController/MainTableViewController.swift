dbc312d4280b58031e27d2a499588f5c8f41612b
gutaeho <sbpdt1062@gmail.com>
2023-06-18 02:23:19 +0900

    정렬 옵션 및 검색 결과 필터 추가

--- Project/MySearchController/MySearchController/MainTableViewController.swift
+++ Project/MySearchController/MySearchController/MainTableViewController.swift
@@ -9,7 +9,15 @@ import UIKit
 
 // MARK: 테이블 형태 구현 - UIViewController 내부에 delegate 방식으로 테이블 뷰를 구현하는 방식이 있고, UITableViewController 의 메소드를 오버라이딩하여 구현하는 방식이 있음
 class MainTableViewController: UITableViewController {
-    var items: [MainTableViewCellItem]?
+    private var items: [MainTableViewCellItem]?
+    private var searchedItems: [MainTableViewCellItem]?
+    
+    // 접두사 일치 시
+    private let SORT_AS_PREFIX = 0
+    // 포함 시
+    private let SORT_AS_CONTAINS = 1
+    
+    private var isShowSearchedItems: Bool = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -49,18 +57,30 @@ class MainTableViewController: UITableViewController {
         /* 힌트 추가 */
         searchController.searchBar.placeholder = "검색어를 입력 해 주세요."
         /* 스코프 바 추가 */
-        searchController.searchBar.scopeButtonTitles = ["번호로 찾기", "고유번호로 찾기"]
+        searchController.searchBar.scopeButtonTitles = ["앞 문자 일치 시", "문자 일치 시"]
         /* 스코프 바 항상 표시 */
         searchController.searchBar.showsScopeBar = true
         /* 검색 시 헤더 타이틀 표시 */
         searchController.hidesNavigationBarDuringPresentation = false
+        /* 검색 결과 델리게이트 위임 */
+        searchController.searchResultsUpdater = self
         /* 스크롤 시 검색바 유지 */
-        navigationItem.hidesSearchBarWhenScrolling = false
+        navigationItem.hidesSearchBarWhenScrolling = true
         navigationItem.searchController = searchController
     }
     
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        
+        view.layoutIfNeeded()
+    }
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return items?.count ?? 0
+        if isShowSearchedItems {
+            return searchedItems?.count ?? 0
+        } else {
+            return items?.count ?? 0
+        }
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
@@ -86,8 +106,41 @@ class MainTableViewController: UITableViewController {
 // MARK: 검색바 결과가 변경될 때 호출
 extension MainTableViewController: UISearchResultsUpdating {
     func updateSearchResults(for searchController: UISearchController) {
-        guard let filteredText = searchController.searchBar.text else { return }
+        guard let text = searchController.searchBar.text?.lowercased() else { return }
         
-        // TODO:
+        // 빈 값일 때는 모든 아이템 표시
+        if text == "" {
+            isShowSearchedItems = false
+            tableView.reloadData()
+            return
+        }
+        
+        
+        let items = items?.filter({
+            switch searchController.searchBar.selectedScopeButtonIndex {
+            case SORT_AS_PREFIX:
+                // 접두사가 일치하는지 검사
+                return $0.content?.lowercased().hasPrefix(text) ?? false
+                
+            case SORT_AS_CONTAINS:
+                // localizedCaseInsensitiveContains() - 대소문자를 구분하지 않고, 해당 문자가 포함되어있는지 검사
+                return $0.content?.localizedCaseInsensitiveContains(text) ?? false
+                
+            default:
+                return false
+            }
+        })
+        
+        // 결과없음
+        guard let items = items, items.count > 0 else {
+            searchedItems = nil
+            isShowSearchedItems = true
+            tableView.reloadData()
+            return
+        }
+        
+        searchedItems = items
+        isShowSearchedItems = true
+        tableView.reloadData()
     }
 }
