8d94f1ff0ab00605365a52e44baab6474cd3c534
gutaeho <sbpdt1062@gmail.com>
2023-06-21 14:17:39 +0900

    - 외부 앱 열기 예제 잘못된 내용 수정

--- Project/MyExternalAppOpen/MyExternalAppOpen/ViewController.swift
+++ Project/MyExternalAppOpen/MyExternalAppOpen/ViewController.swift
@@ -8,10 +8,17 @@
 import UIKit
 
 /**
- 외부 앱 열기 예제
+ 외부 앱  to 현재 앱 열기 예제
+ - 현재 앱에서 작업해야 할 것
  1. MyExternalAppOpen.xcodeproj > Target > URL Types 열기
- 2. 열고 싶은 앱의 Identifier(ex -> com.example.MyExternalAppOpen) 와 URL Schemes (ex -> myExternalApp) 작성 후 + 버튼으로 추가
- 3. 방금 작성한 URL Schemes 를 UIApplication.shared.open(string: String) 메소드로 열기
+ 2. 현재 앱의 Identifier(ex -> com.example.MyExternalAppOpen) 와 URL Schemes (ex -> myExternalApp) 작성 후 + 버튼으로 추가
+ - 외부 앱에서 작업해야 할 것
+ 1. (외부 앱의) info.plist 에 LSApplicationQueriesSchemes 키 추가 (권한 허용)
+ 2. 방금 작성한 URL Schemes 를 UIApplication.shared.open(_: 메소드로 열기
+ 
+ 
+ 현재 앱 to 외부 앱 열기 예제
+ 1. 외부 앱에서 작업해야 할 것과 동일
  */
 // MARK: 브라우저 주소창에 스키마명:// (ex >> myExternalApp://) 입력 시 현재 앱 실행됨
 class ViewController: UIViewController {
@@ -23,8 +30,9 @@ class ViewController: UIViewController {
     }
 
     @IBAction func appOpen(_ sender: Any) {
-        let appURL = URL(string: "daeguro://daeguro?project=DELIVERY")!
+//        let appURL = URL(string: "daeguro://daeguro?project=DELIVERY")!
         let webURL = URL(string: "http://naver.com")!
+        let appURL = URL(string: "mhbanking://dgr")!
         
         if UIApplication.shared.canOpenURL(appURL) {
             UIApplication.shared.open(appURL, options: [:], completionHandler: nil)
