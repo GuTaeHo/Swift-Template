0cd042c0b93453d83d4383c79b42d1197ef4c4a6
gutaeho <sbpdt1062@gmail.com>
2023-08-06 23:14:32 +0900

    - UIKeyboardLayoutGuide 사용법 추가

--- Project/MySafeArea/MySafeArea/ViewController.swift
+++ Project/MySafeArea/MySafeArea/ViewController.swift
@@ -7,48 +7,75 @@
 
 import UIKit
 
-// 현재 기기의 상,하,좌,우 SafeArea 확인 예제
+// MARK: iOS 15 부터 새로추가된 UIKeyboardLayoutGuide 를 사용하면 이전의 복잡한 키보드 UI 처리방법 대신, 자동으로 처리를 해준다.
 class ViewController: UIViewController {
     @IBOutlet var lbSafeAreas: [UILabel]!
     
-    private let TOP = 0
-    private let LEFT = 1
-    private let RIGHT = 2
-    private let BOTTOM = 3
+    private lazy var idTextField: UITextField = {
+        var textField = UITextField()
+        textField.placeholder = " ID"
+        textField.layer.borderColor = UIColor.gray.cgColor
+        textField.layer.borderWidth = 1.0
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        return textField
+    }()
     
+    private lazy var passwordTextField: UITextField = {
+        var textField = UITextField()
+        textField.placeholder = " Password"
+        textField.layer.borderColor = UIColor.gray.cgColor
+        textField.layer.borderWidth = 1.0
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        return textField
+    }()
+    
+    private lazy var loginButton: UIButton = {
+        var button = UIButton()
+        button.setTitle("Login", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = .blue
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .secondarySystemBackground
         
-        for index in 0..<lbSafeAreas.count {
-            lbSafeAreas[index].text = "\(safeArea(direction: index) ?? 0)"
-        }
+        view.addSubview(idTextField)
+        view.addSubview(passwordTextField)
+        view.addSubview(loginButton)
+        
+        loginButton.addTarget(self, action: #selector(login), for: .touchDown)
+        
+        NSLayoutConstraint.activate([
+            idTextField.widthAnchor.constraint(equalToConstant: 300),
+            idTextField.heightAnchor.constraint(equalToConstant: 30),
+            idTextField.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            idTextField.topAnchor.constraint(equalTo: view.topAnchor, constant: 100),
+            
+            passwordTextField.widthAnchor.constraint(equalToConstant: 300),
+            passwordTextField.heightAnchor.constraint(equalToConstant: 30),
+            passwordTextField.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            passwordTextField.topAnchor.constraint(equalTo: idTextField.bottomAnchor, constant: 20),
+            
+            /*
+            loginButton.widthAnchor.constraint(equalTo: view.widthAnchor),
+            loginButton.heightAnchor.constraint(equalToConstant: 50),
+            loginButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            loginButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -50)
+             */
+            
+            // 하단 제약조건 UIKeyboardLayoutGuide의 상단과 맞춤
+            loginButton.widthAnchor.constraint(equalTo: view.widthAnchor),
+            loginButton.heightAnchor.constraint(equalToConstant: 50),
+            loginButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            loginButton.bottomAnchor.constraint(equalTo: view.keyboardLayoutGuide.topAnchor, constant: -50)
+        ])
     }
     
-    private func safeArea(direction: Int) -> CGFloat? {
-        guard
-            let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
-            let safeAreaInsets = windowScene.windows.first?.safeAreaInsets else
-        {
-            return nil
-        }
-        
-        switch direction {
-        case TOP:
-            return safeAreaInsets.top
-            
-        case LEFT:
-            return safeAreaInsets.left
-            
-        case RIGHT:
-            return safeAreaInsets.right
-            
-        case BOTTOM:
-            return safeAreaInsets.bottom
-            
-        default:
-            return nil
-        }
+    @objc func login() {
+        view.endEditing(true)
     }
 }
 
