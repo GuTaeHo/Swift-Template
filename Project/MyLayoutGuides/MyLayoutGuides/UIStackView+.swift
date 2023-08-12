dded898e2f5c3b6a2d2f1748c4e81cc3568f8896
gutaeho <sbpdt1062@gmail.com>
2023-08-12 16:17:43 +0900

    - exemple of Dynamically removing views in a Stack View

---
+++ Project/MyLayoutGuides/MyLayoutGuides/UIStackView+.swift
@@ -0,0 +1,30 @@
+//
+//  UIStackView+.swift
+//  MyLayoutGuides
+//
+//  Created by 구태호 on 2023/08/12.
+//
+
+import UIKit
+
+/**
+    arranged 된 서브뷰를 스택뷰 내부에서 제거할 때 removeArrangedSubview() 를 사용하면된다.
+    removeArrangedSubview() 는 UI는 제거되지만, 실제로 스택 뷰 계층에서는 사라지지 않고 남아있는 것을 볼 수 있는데
+    이를 해결하기 위해서 removeFromSuperView() 메소드를 사용하여 뷰 계층에서 완전히 지워줘야한다.
+*/
+extension UIStackView {
+    /// view 를 스택 뷰에서 제거
+    func removeFromStackView(view: UIView?) {
+        guard let view = view else { return }
+        
+        removeArrangedSubview(view)
+        view.removeFromSuperview()
+    }
+    
+    /// 스택 뷰 하위 모든 뷰 제거
+    func removeAllViewsInStackView() {
+        arrangedSubviews.forEach { (view) in
+            removeFromStackView(view: view)
+        }
+    }
+}
--- Project/MyLayoutGuides/MyLayoutGuides/ViewController.swift
+++ Project/MyLayoutGuides/MyLayoutGuides/ViewController.swift
@@ -26,9 +26,48 @@ class ViewController: UIViewController {
         
         return label
     }()
+    
+    lazy var layoutMarginsTestStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.backgroundColor = .gray
+        stackView.axis = .vertical
+        stackView.layer.cornerRadius = 12
+        stackView.layoutMargins = UIEdgeInsets(top: 12, left: 18, bottom: 12, right: 18)
+        // MARK: 스택뷰의 레이아웃 마진 활성화
+        stackView.isLayoutMarginsRelativeArrangement = true
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.spacing = 12
+        
+        return stackView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        view.addSubview(layoutMarginsTestStackView)
+        
+        layoutMarginsTestStackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+        layoutMarginsTestStackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
+        layoutMarginsTestStackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
+        
+        for _ in 0...5 {
+            let layoutMarginsTestViewInStackView: UIView = {
+                let view = UIView()
+                view.layer.cornerRadius = 12
+                view.backgroundColor = .systemIndigo
+                view.translatesAutoresizingMaskIntoConstraints = false
+                view.layoutMargins = .init(top: 6, left: 12, bottom: 6, right: 12)
+               
+                return view
+            }()
+            
+            layoutMarginsTestStackView.addArrangedSubview(layoutMarginsTestViewInStackView)
+            layoutMarginsTestViewInStackView.heightAnchor.constraint(equalToConstant: 80).isActive = true
+            // 뷰에 제스쳐 등록
+            layoutMarginsTestViewInStackView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(clickedViewInStackview(_:))))
+        }
+        
+        
         
         // MARK: 스크롤 뷰의 contentInset 처럼 UIView 는 layoutMargins 속성이 존재
         // MARK: 해당 속성을 통해 뷰와 콘텐츠 사이에 패딩을 넣어줄 수 있다.
@@ -46,6 +85,13 @@ class ViewController: UIViewController {
             layoutMarginsTestView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             layoutMarginsTestView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
         ])
+    }
+    
+    // 스택 뷰에서 제거
+    @objc func clickedViewInStackview(_ sender: UITapGestureRecognizer) {
+        // dump(sender)
+        layoutMarginsTestStackView.removeFromStackView(view: sender.view)
+        // layoutMarginsTestStackView.removeAllViewsInStackView()
     }
 }
 
