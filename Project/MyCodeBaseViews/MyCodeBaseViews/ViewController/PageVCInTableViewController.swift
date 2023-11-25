0b73eb76e70902cbeaa52640b809384bae2f2a81
gutaeho <sbpdt1062@gmail.com>
2023-11-25 16:44:16 +0900

    - 주석 추가

--- Project/MyCodeBaseViews/MyCodeBaseViews/ViewController/PageVCInTableViewController.swift
+++ Project/MyCodeBaseViews/MyCodeBaseViews/ViewController/PageVCInTableViewController.swift
@@ -7,6 +7,21 @@
 
 import UIKit
 
+/**
+ 섹션 헤더가 탭 바 형태인 뷰 컨트롤러 구현
+ 아이디어
+  1 안
+ 1. 테이블 뷰 섹션과 셀은 각각 1개씩 존재
+ 2. 섹션은 3개의 탭으로 구성되며, 셀은 PageViewController 를 가짐
+ 3. 셀의 높이는 기기 높이 - 상단 바 높이 + 섹션 헤더 높이
+ 4. 가장 큰 문제는 외부 테이블 뷰와 내부 테이블 뷰의 스크롤이 중첩될 듯 하다. 특정 Offset 에 도달하는순간 스크롤을 스위칭해주어야 할 듯하다.
+ 
+  2 안
+ 1. 테이블 뷰 섹션과 셀은 각각 1개씩 존재
+ 2. 섹션은 3개의 탭으로 구성되며, 셀은 PageViewController 를 가짐
+ 3. 셀의 높이는 기기 높이 - 상단 바 높이 + 섹션 헤더 높이
+ 4. 셀의 각 뷰 컨트롤러가 표시될 때 `페이저 뷰 컨트롤러 내부의 테이블 뷰 높이를 갱신` 해야함
+ */
 
 class PageVCInTableViewController: BaseViewController {
     let sectionTitles = ["메뉴", "정보", "리뷰"]
@@ -16,6 +31,10 @@ class PageVCInTableViewController: BaseViewController {
         // MARK: cellClass 는 반드시 UITableViewCell 을 서브클래싱하는 클래스여야한다.
         $0.register(MenuTableViewCell.self, forCellReuseIdentifier: MenuTableViewCell.className)
     }
+    
+//    let pageViewController = UIPageViewController(transitionStyle: .scroll, navigationOrientation: .horizontal) {
+//        $0.setViewCon
+//    }
     
     
     override func viewDidLoad() {
