f5575a339078eaaebe61db2ca287853b9d079cee
gutaeho <sbpdt1062@gmail.com>
2023-12-19 11:19:44 +0900

    - URL Scheme 호출 및 받는 쪽 예제 추가

---
+++ Project/MyUrlSchemeOpener/MyUrlSchemeOpener/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  MyUrlSchemeOpener
+//
+//  Created by 구태호 on 12/19/23.
+//
+
+import UIKit
+
+/**
+ URL Scheme 방식으로 앱 여는 방법 (여는 쪽)
+ 1. `info.plist` 에 `Queried URL Schemes` 속성을 추가한다.
+ 2. 열고 싶은 앱의 URL Scheme 을 item 의 value 에 입력한다.
+ 3. `UIApplication.shared.canOpenUrl(_: URL)` 함수를 통해 앱을 열 수 있는지 검사한다.
+ 4. `UIApplication.shared.open(_: URL)` 함수로 앱을 연다.
+ */
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    @IBAction func buttonHandler(_ sender: UIButton) {
+        guard let url = URL(string: "myurlschemereceiver://") else {
+            return
+        }
+        
+        if UIApplication.shared.canOpenURL(url) {
+            UIApplication.shared.open(url)
+        }
+    }
+}
+
---
+++ Project/MyUrlSchemeReceiver/MyUrlSchemeReceiver/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  MyUrlSchemeReceiver
+//
+//  Created by 구태호 on 12/19/23.
+//
+
+import UIKit
+import Combine
+
+/**
+ URL Scheme 방식으로 앱 여는 방법 (열리는 쪽)
+ 1. 프로젝트 > Target > Info > URL Types 에 `시스템이 인식`할 자신의(열릴) 앱의 URL Scheme 을 지정한다.
+ 2. AppDelegate 만 사용중인 프로젝트일 경우,
+    `application(_: UIApplication, open: URL, options: [UIApplication.OpenURLOptionsKey : Any]) -> Bool`
+    를 구현하면, open 인자로 url 정보가 들어오며 핸들링이 가능하다.
+ 3. SceneDelegate 를 함께 사용하는 프로젝트의 경우 (이 경우 AppDelegate 에 구현된 open 메소드는 호출되지 않는다),
+    `scene(_: UIScene, openURLContexts: Set<UIOpenURLContext>)` 를 구현해준 뒤, 들어오는 url 목록을 핸들링 해주면 된다.
+ 4.
+ */
+
+class ViewController: UIViewController {
+    private var cancellable = Set<AnyCancellable>()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        NotificationCenter.default.publisher(for: NSNotification.Name(rawValue: "arrivedUrl")).sink { notification in
+            print("\(notification)")
+        }.store(in: &cancellable)
+    }
+}
+
