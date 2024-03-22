d5b48d472d5597d3965a07fde84f2ef4d4d5fdc3
GuTaeho <sbpdt1062@gmail.com>
2024-03-22 21:40:49 +0900

    - XCFramework 생성 및 적용 예제 추가

---
+++ Project/MyFirstFrameworkUse/MyFirstFrameworkUse/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyFirstFrameworkUse
+//
+//  Created by 구태호 on 3/20/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
