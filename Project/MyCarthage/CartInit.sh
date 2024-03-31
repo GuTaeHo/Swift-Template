78aac1947d63b60a6431d5c0b25031b9059d1240
GuTaeho <sbpdt1062@gmail.com>
2024-03-31 23:39:01 +0900

    - 카르타고 예제 추가

---
+++ Project/MyCarthage/CartInit.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# 카르타고로 모든 프레임워크를 초기화 하기 위한 실행파일입니다.
+# [주의] 애플 실리콘이 장착된 컴퓨터에
+
+
+# 1. Carthage.sh 파일이 없을 경우 생성
+# 2. 생성된 Carthage.sh 파일과 함께 명령어 실행
+#    ./carthage.sh update --use-xcframeworks
+
