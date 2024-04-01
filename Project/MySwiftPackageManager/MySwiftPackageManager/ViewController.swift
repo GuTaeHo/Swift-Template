fa14ef3f749c64328178f460de543382e9cee004
GuTaeho <sbpdt1062@gmail.com>
2024-04-01 23:25:53 +0900

    - 스위프트 패키지 매니저 예제 프로젝트 추가

---
+++ Project/MySwiftPackageManager/MySwiftPackageManager/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  MySwiftPackageManager
+//
+//  Created by 구태호 on 4/1/24.
+//
+
+import UIKit
+import Then
+import SnapKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        
+    }
+}
+
