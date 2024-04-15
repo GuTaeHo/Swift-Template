486e26c12dc4a9bf193a8594739d55a5ee9c2aea
gutaeho_macbook_air <sbpdt1062@gmail.com>
2024-04-15 14:53:19 +0900

    - 키보드 퍼블리셔 추가

--- Project/MyKeyboardNotification/MyKeyboardNotification/extension/UIViewController.swift
+++ Project/MyKeyboardNotification/MyKeyboardNotification/extension/UIViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Combine
 
 extension UIViewController {
     /// 키보드 옵저버 등록
@@ -20,5 +21,29 @@ extension UIViewController {
     func removeKeyboardObserver() {
         NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
         NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    /// 키보드 높이가 변경될 경우 키보드 높이를 방출하는 퍼블리셔를 생성합니다.
+    var keyboardPublisher: AnyPublisher<CGFloat, Never> {
+        let willShowPublisher = NotificationCenter.default.publisher(for: UIResponder.keyboardWillShowNotification)
+        let willHidePublisher = NotificationCenter.default.publisher(for: UIResponder.keyboardWillHideNotification)
+        
+        let mergedPublisher = willShowPublisher
+            .merge(with: willHidePublisher)
+            .compactMap { notification in
+                guard
+                    let keyboardFrame: NSValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue,
+                    let screenHeight = UIApplication.shared.delegate?.window??.screen.bounds.size.height, screenHeight > 0
+                else {
+                    return CGFloat(0)
+                }
+                
+                if keyboardFrame.cgRectValue.minY < screenHeight {
+                    return keyboardFrame.cgRectValue.height
+                } else {
+                    return CGFloat(0)
+                }
+            }
+        return mergedPublisher.eraseToAnyPublisher()
     }
 }
