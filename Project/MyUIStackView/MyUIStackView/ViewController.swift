a3d5e82f54dd63bf7be4c766119f5b4e349653a2
gutaeho_macbook_air <sbpdt1062@gmail.com>
2024-06-04 09:45:47 +0900

    Feat: 높이가 지정되지않은 UIStackView 에 alignment 가 .bottom 일 때, 스택뷰의 높이는 가장 높은 서브뷰의 높이와 같아지는지 예제 추가

--- Project/MyUIStackView/MyUIStackView/ViewController.swift
+++ Project/MyUIStackView/MyUIStackView/ViewController.swift
@@ -12,10 +12,21 @@ class ViewController: UIViewController {
     lazy var stackView = UIStackView(axis: .vertical, spacing: 12, margins: .init(horizontal: 12, vertical: 18), subviews: [aView, bView])
     let aView = UIView(backgroundColor: .systemPink)
     let bView = UIView(backgroundColor: .systemIndigo)
+    
+    lazy var dynamicBottomAlignmentStackView = UIStackView(axis: .horizontal, spacing: 12, alignment: .bottom, distribution: .fillEqually, margins: .init(horizontal: 12, vertical: 18), subviews: [cView, dView, eView])
+    
+    let cView = UIView(backgroundColor: .systemGreen)
+    let dView = UIView(backgroundColor: .systemYellow)
+    let eView = UIView(backgroundColor: .systemPink)
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        initStackView()
+        initDynamicBottomAlignmentStackView()
+    }
+    
+    func initStackView() {
         view.addSubview(stackView)
         NSLayoutConstraint.activate([
             stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
@@ -29,6 +40,27 @@ class ViewController: UIViewController {
         
         bView.snp.makeConstraints {
             $0.height.equalTo(80)
+        }
+    }
+    
+    func initDynamicBottomAlignmentStackView() {
+        view.addSubview(dynamicBottomAlignmentStackView)
+        NSLayoutConstraint.activate([
+            dynamicBottomAlignmentStackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            dynamicBottomAlignmentStackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            dynamicBottomAlignmentStackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor)
+        ])
+        
+        cView.snp.makeConstraints {
+            $0.height.equalTo(20)
+        }
+        
+        dView.snp.makeConstraints {
+            $0.height.equalTo(60)
+        }
+        
+        eView.snp.makeConstraints {
+            $0.height.equalTo(40)
         }
     }
 }
