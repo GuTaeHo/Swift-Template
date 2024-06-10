236e6feb69493b495ce648eb78f512c8915cb012
GuTaeho <sbpdt1062@gmail.com>
2024-06-10 23:52:38 +0900

    Feat: 이미지 피커 추가

--- Project/MyPhotoKit/MyPhotoKit/ViewController/ViewController.swift
+++ Project/MyPhotoKit/MyPhotoKit/ViewController/ViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 import Then
 import SnapKit
 import Photos
+import PhotosUI
 
 
 class ViewController: UIViewController {
@@ -23,9 +24,15 @@ class ViewController: UIViewController {
         $0.register(GridCollectionViewCell.self, forCellWithReuseIdentifier: "GridCollectionViewCell")
     }
     
-    let btGetPhotos = UIButton(configuration: .filled()).then {
-        $0.configuration?.title = "사진 가져오기!"
+    let btShowPicker = UIButton(configuration: .filled()).then {
+        $0.configuration?.title = "피커 표시하기"
     }
+    
+    let btGetPhotos = UIButton(configuration: .filled()).then {
+        $0.configuration?.title = "사진 가져오기"
+    }
+    
+    var pickerViewController: PHPickerViewController!
     
     var gridImages = [PHAsset]()
     lazy var manager = PhotosManager(delegate: self)
@@ -40,10 +47,15 @@ class ViewController: UIViewController {
     }
     
     func initViews() {
-        view.addSubviews(btGetPhotos, gridCollectionView)
+        view.addSubviews(btShowPicker, btGetPhotos, gridCollectionView)
         btGetPhotos.snp.makeConstraints {
             $0.top.equalTo(view.safeAreaLayoutGuide)
             $0.trailing.equalToSuperview().inset(10)
+        }
+        
+        btShowPicker.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide)
+            $0.trailing.equalTo(btGetPhotos.snp.leading).offset(-10)
         }
         
         gridCollectionView.snp.makeConstraints {
@@ -60,6 +72,12 @@ class ViewController: UIViewController {
         btGetPhotos.addAction(.init(handler: { [weak self] _ in
             Task { await self?.manager.requestAuthorization }
         }), for: .touchUpInside)
+        
+        btShowPicker.addAction(.init(handler: { [weak self] _ in
+            Task { 
+                self?.showPhotoSelect()
+            }
+        }), for: .touchUpInside)
     }
     
     func initData() {
@@ -68,7 +86,7 @@ class ViewController: UIViewController {
         }
     }
     
-    func moveSetting() {
+    func showSettingAlert() {
         let alertController = UIAlertController(title: "권한 거부됨", message: "앨범 접근이 거부 되었습니다.", preferredStyle: UIAlertController.Style.alert)
         
         let okAction = UIAlertAction(title: "설정으로 이동", style: .default) { (action) in
@@ -87,6 +105,40 @@ class ViewController: UIViewController {
         Task {
             self.present(alertController, animated: true, completion: nil)
         }
+    }
+    
+    func showImagePickerVC() {
+        let alertController = UIAlertController(title: "권한 제한됨", message: "일부 앨범 접근만 가능합니다.", preferredStyle: UIAlertController.Style.alert)
+        let showPicker = UIAlertAction(title: "이미지 피커 표시", style: .default) { (action) in
+            self.showPhotoSelect()
+        }
+        let moveSetting = UIAlertAction(title: "설정으로 이동", style: .default) { (action) in
+            guard let settingsUrl = URL(string: UIApplication.openSettingsURLString) else {
+                return
+            }
+            if UIApplication.shared.canOpenURL(settingsUrl) {
+                UIApplication.shared.open(settingsUrl)
+            }
+        }
+        let cancel = UIAlertAction(title: "확인", style: .cancel, handler: nil)
+        
+        alertController.addAction(showPicker)
+        alertController.addAction(moveSetting)
+        alertController.addAction(cancel)
+        
+        Task {
+            self.present(alertController, animated: true, completion: nil)
+        }
+    }
+    
+    func showPhotoSelect() {
+        var configuration = PHPickerConfiguration(photoLibrary: PHPhotoLibrary.shared())
+        configuration.selectionLimit = 5
+        configuration.filter = .images
+        
+        pickerViewController = PHPickerViewController(configuration: configuration)
+        pickerViewController.delegate = self
+        self.present(self.pickerViewController, animated: true, completion: nil)
     }
 }
 
@@ -126,7 +178,6 @@ extension ViewController: PhotosDelegate {
                 self.gridCollectionView.reloadData()
             }
         case .limited:
-            // TODO: PHPickerViewController 로 이미지 가져오기 구현
             Task {
                 let photos = await manager.photos
                 if let photos {
@@ -135,9 +186,36 @@ extension ViewController: PhotosDelegate {
                 self.gridCollectionView.reloadData()
             }
         case .denied:
-            moveSetting()
+            showSettingAlert()
         default: break
         }
+    }
+}
+
+extension ViewController: PHPickerViewControllerDelegate  {
+    func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+        picker.dismiss(animated: true, completion: nil)
+        
+        let identifiers = results.compactMap(\.assetIdentifier)
+        // identifier 를 통해 에셋목록 획득
+        let fetchResult = PHAsset.fetchAssets(withLocalIdentifiers: identifiers, options: nil)
+        fetchResult.enumerateObjects { [weak self] (asset, index, stop) -> Void in
+            self?.gridImages.insert(asset, at: 0)
+            self?.gridCollectionView.reloadData()
+        }
+        //        let itemProviders = results.map(\.itemProvider)
+        //        itemProviders.filter {
+        //            $0.canLoadObject(ofClass: UIImage.self)
+        //        }.forEach {
+        //            $0.loadObject(ofClass: UIImage.self) { [weak self] (image, error) in
+        //                DispatchQueue.main.async {
+        //
+        //                    if let image = image as? UIImage {
+        //
+        //                    }
+        //                }
+        //            }
+        //        }
     }
 }
 
