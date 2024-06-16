ece5ad49c5c0208250688bbfcbfbb40c8797ae4a
GuTaeho <sbpdt1062@gmail.com>
2024-06-17 01:04:37 +0900

    Feat: MyFirstLibray 예제 추가

---
+++ Project/MyFirstLibrary/MyFirstLibrary/MyFirstLibrary.swift
@@ -0,0 +1,14 @@
+//
+//  MyFirstLibrary.swift
+//  MyFirstLibrary
+//
+//  Created by 구태호 on 6/16/24.
+//
+
+class MyFirstLibrary {
+    
+    /// 두 수의 합을 반환합니다.
+    func plus(lhs: Int, rhs: Int) -> Int {
+        return lhs + rhs
+    }
+}
