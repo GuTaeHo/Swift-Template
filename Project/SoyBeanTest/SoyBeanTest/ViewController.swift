9c9248a5ea9d927542a90cdd89f5557e71a8d34f
GuTaeho <sbpdt1062@gmail.com>
2024-10-02 00:29:02 +0900

    Chore: SoyBean 모듈 테스트 용 SoyBeanTest 프로젝트 추가

---
+++ Project/SoyBeanTest/SoyBeanTest/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  SoyBeanTest
+//
+//  Created by 구태호 on 10/2/24.
+//
+
+import UIKit
+import SoyBeanUI
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        SoyBeanUI().test()
+    }
+
+
+}
+
