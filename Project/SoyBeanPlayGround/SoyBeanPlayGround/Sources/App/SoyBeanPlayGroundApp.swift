5b3dfdce1c6f019a93b30919f699bc8b8bfd1929
gutaeho_macbook_air <sbpdt1062@gmail.com>
2025-06-11 10:22:36 +0900

    Chore: 디렉토리 구성 변경

---
+++ Project/SoyBeanPlayGround/SoyBeanPlayGround/Sources/App/SoyBeanPlayGroundApp.swift
@@ -0,0 +1,31 @@
+import SwiftUI
+import SoyBeanCore
+import SoyBeanUI
+
+@main
+struct SoyBeanPlayGroundApp: App {
+    init() {
+        print("폰트 등록: \(Font.registerFonts())")
+        Log.debug("Hello, Debug!")
+        Log.info("Hello, Info!")
+        print("앱 명: \(Bundle.main.appName)")
+        print("앱 번들: \(Bundle.main.appBundleID)")
+        print("앱 버전: \(Bundle.main.appVersion)")
+        print("앱 빌드 번호: \(Bundle.main.appBuildNumber)")
+        print("앱 빌드 환경: \(Bundle.main.appInstallEnvironment.name)")
+    }
+    
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                List {
+                    NavigationLink("롱프레스 뷰", destination: LongPressView())
+                    NavigationLink("햅틱 뷰", destination: HapticView())
+                    NavigationLink("숫자 애니매이션 카운트 뷰", destination: NumberAnimationView())
+                    NavigationLink("SDWebImageView", destination: GIFImageTestView())
+                }
+                .navigationTitle("메인 뷰")
+            }
+        }
+    }
+}
---
+++ Project/SoyBeanPlayGround/SoyBeanPlayGround/Sources/Subviews/NumberAnimationView.swift
@@ -0,0 +1,33 @@
+//
+//  NumberAnimationView.swift
+//  SoyBeanPlayGround
+//
+//  Created by 구태호 on 4/29/25.
+//
+
+import SwiftUI
+import SoyBeanUI
+import SoyBeanUtil
+
+struct NumberAnimationView: View {
+    let timer = Timer.publish(every: 1,
+                              on: .main,
+                              in: .common).autoconnect()
+    @State private var numberCount: Int = 0
+    
+    var body: some View {
+        NumberAnimationTextView(font: .custom(.IBMPlexBold,
+                                              size: 30),
+                                number: numberCount)
+            .onReceive(timer) { _ in
+                withAnimation {
+                    numberCount += Int.random(in: 0..<10)
+                }
+            }
+        Button(action: {
+            Util.exitApp()
+        }) {
+            Text("앱 종료")
+        }
+    }
+}
