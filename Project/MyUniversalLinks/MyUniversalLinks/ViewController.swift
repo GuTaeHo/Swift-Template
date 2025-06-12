b6041b1b7aa709a55837de7885263a91a1c2f724
GuTaeho <sbpdt1062@gmail.com>
2025-06-12 23:49:25 +0900

    Feat: MyUniversalLinks 앱 추가 (유니버셜 링크 구현 X)

---
+++ Project/MyUniversalLinks/MyUniversalLinks/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyUniversalLinks
+//
+//  Created by 구태호 on 6/12/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
